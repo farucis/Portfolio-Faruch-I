11b88353b5baeb7ac1ebff28f6031eb177e59b04
faruch ismailov <58651410+farucis@users.noreply.github.com>
2022-07-15 04:44:42 +0300

    add About Me component

---
+++ src/ProtfolioContainer/AboutME/AboutCard/AboutCard.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./AboutCard.css";
+
+const AboutCard = () => {
+  const infoText =
+    "Full stack web and mobile developer with ability to work under pressure and highly motivated for new knowledge in the programming industry. Responsible, fast learner and a team player ,along with a  knack of building applications with utmost efficiency. Seeking a position in software development that will utilize my knowledge in python, Javascript, React, React-Native development and my C/C++/python/Java programming skills. Strong professional with a BSC willing to be an asset for an organization.";
+  const detailsText = [
+    "Full Stack web and mobile development",
+    "Interactive Front End as per the design",
+    "React and React Native",
+    "Redux for State Mnanagement",
+    "Building REST API",
+    "Managing database",
+  ];
+
+  return (
+    <div className="aboutCard-container">
+      <div className="aboutCard-parent">
+
+        <div className="aboutCard-image" />
+
+        <div className="aboutCard-text-container">
+            
+          <div className="aboutCard-text-info">
+            <p>{infoText}</p>
+          </div>
+          <div className="aboutCard-text-details">
+            <h1>Here are a Few Highlights</h1>
+            {detailsText.map((detail) => (
+              <div className="detail-option">
+                🔴
+                <h2>{detail}</h2>
+              </div>
+            ))}
+          </div>
+          <div className="About-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire Me{" "}
+            </button>
+            <a href="Faruch - CV.pdf" download="Faruch Ismailov CV">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default AboutCard;
--- src/ProtfolioContainer/AboutME/AboutME.js
+++ src/ProtfolioContainer/AboutME/AboutME.js
@@ -1,12 +1,13 @@
 import React from "react";
 import TitleHeader from "../Header/TitleHeader";
+import AboutCard from "./AboutCard/AboutCard";
 
 const AboutME = () => {
   return (
     <div className="about-me-container">
       <div className="about-me-parent">
         <TitleHeader title="About Me" subTitle="Why Choose Me?" />
-
+        <AboutCard />
       </div>
     </div>
   );
