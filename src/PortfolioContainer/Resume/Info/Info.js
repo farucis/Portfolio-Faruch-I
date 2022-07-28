34188e45a9ed7c52caa46b09730720fa4be0cf86
faruch ismailov <58651410+farucis@users.noreply.github.com>
2022-07-28 04:33:49 +0300

    Update Info.js

--- src/PortfolioContainer/Resume/Info/Info.js
+++ src/PortfolioContainer/Resume/Info/Info.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Info.css";
+
 import Education from "./Education/Education";
 import WorkHistory from "./WorkHistory/WorkHistory";
 import Skills from "./Skills/Skills";
@@ -8,21 +9,27 @@ import Interests from "./Interests/Interests";
 
 const Info = () => {
   return (
-    <div className="resum-info">
-      <div className="scroller">
-        <section className="resum-info-education" id="education">
+    <div className="col-8">
+      <div
+        data-bs-spy="scroll"
+        data-bs-target="#list-example"
+        data-bs-smooth-scroll="true"
+        className="scrollspy"
+        tabIndex="0"
+      >
+        <section style={{ height: "100%" }} id="education">
           <Education />
         </section>
-        <section className="resum-info-workHistory" id="workHistory">
+        <section style={{ height: "100%" }} id="workHistory">
           <WorkHistory />
         </section>
-        <section className="resum-info-skills" id="programmingSkills">
+        <section style={{ height: "100%" }} id="programmingSkills">
           <Skills />
         </section>
-        <section className="resum-info-projects" id="reprojects">
+        <section style={{ height: "100%" }} id="reprojects">
           <Projects />
         </section>
-        <section className="resum-info-interests" id="interests">
+        <section style={{ height: "100%" }} id="interests">
           <Interests />
         </section>
       </div>
